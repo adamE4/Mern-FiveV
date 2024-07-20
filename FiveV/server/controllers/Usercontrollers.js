2b326b1b1d6fdf8aceb0469336d68b55ce655abf
adamE4 <adamelmobdy@gmail.com>
2024-07-20 12:16:34 -0700

    created LoginUser and JWT for signup

--- FiveV/server/controllers/Usercontrollers.js
+++ FiveV/server/controllers/Usercontrollers.js
@@ -2,9 +2,32 @@
 import bcrypt from "bcryptjs"
 import { User } from "../models/Usermodel.js"
 import validator from "validator"
+import { Jwt } from "jsonwebtoken"
+
+const createToken = (_id) =>{
+    return Jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'}) //taking the user._id and the secret value from our .env file to create a JWT for the user
+}
+
 
 export const loginUser = async (req, res) =>{
-    res.json({mssg: 'Login User'})
+    const { email, password } = req.body;
+
+    const user = await User.find(email) //Finds email to find User
+
+    if(!user){
+        res.status(401).json({ error: 'Invalid'})
+    }
+
+    const hashed = user.password 
+
+    const isPasswordValid = await bcrypt.compare(password, hashed) // comparing the password passed by the client to the password from the user account we found to verify
+
+    if(isPasswordValid){
+        res.status(200).json({message: 'Login Successful'})
+    }
+    else{
+        res.status(401).json({ error: 'Invalid'})
+    }
 }
 
 export const signupUser = async (req, res) =>{
@@ -22,13 +45,15 @@ export const signupUser = async (req, res) =>{
 
 
 
-    const salt = await bcrypt.genSalt(10)
-    const hashed = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(10) //salting the password which adds random characters to the password
+    const hashed = await bcrypt.hash(password, salt) // Using the salt and the password passed by the client and then hashinng it for added secruity
 
     try{
-        const user = await User.create({ email, password: hashed})
+        const user = await User.create({ email, password: hashed}) //creating user
 
-        res.status(200).json(user)
+        const token = createToken(user._id) //creates JWT for user
+
+        res.status(200).json({email, token})
     }catch(error){
         res.status(400).json({error: error.message})
     }
