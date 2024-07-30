2c22df98e3a58416174bc8890f0424fc347e5213
adamE4 <adamelmobdy@gmail.com>
2024-07-29 20:25:43 -0700

    Protected Frontend Routes

--- FiveV/client/src/App.jsx
+++ FiveV/client/src/App.jsx
@@ -1,6 +1,6 @@
 //Baiscally MAIN offically the root component
 //roots are building blocks this where the castle gets built
-import { BrowserRouter, Outlet } from "react-router-dom";
+import { BrowserRouter, Navigate, Outlet } from "react-router-dom";
 import Viewpost from "./components/viewPost"
 import Posts from "./components/CreatePost"
 import Home from "./components/Homepage"
@@ -8,9 +8,13 @@ import {Route, Routes} from "react-router-dom";
 import Preview from "./components/PreviewPage";
 import LoginForm from "./pages/Login";
 import SignUpForm from "./pages/Signup";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 
 const App = () =>{
+
+    const { user } = useAuthContext()
+
     return(
         <div className="App">
 
@@ -23,24 +27,24 @@ const App = () =>{
 
             <Route 
                 path="/home" 
-                element={<Home />} 
+                element={user ? <Home /> : <Navigate to="/login"/>} 
             />
             
             <Route 
                 path="/view" 
-                element={<Viewpost />} 
+                element={user ? <Viewpost /> : <Navigate to="/login"/>} 
             />
             <Route 
                 path="/posts" 
-                element={<Posts />} 
+                element={user ? <Posts /> : <Navigate to="/login"/>} 
             />
              <Route 
                 path="/SignUp" 
-                element={<SignUpForm />} 
+                element={!user ? <SignUpForm /> : <Navigate to="/home"/>} 
             />
             <Route 
                 path="/Login" 
-                element={<LoginForm />} 
+                element={!user ? <LoginForm /> : <Navigate to="/home"/>} 
             />
 
             </Routes>
